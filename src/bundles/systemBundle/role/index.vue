7685e7c5f026bf980cc445894dbdf89a9476f84c
yangchao <395709515@qq.com>
2020-03-31 13:31:07 +0800

    增加角色管理

--- src/bundles/systemBundle/role/index.vue
+++ src/bundles/systemBundle/role/index.vue
@@ -1,8 +1,44 @@
 <template>
-  <div>SystemRoleIndex</div>
+  <div class="jc-main-full-container">
+    <org-trees class="jc-trees-warp" @node-change="nodeChange"></org-trees>
+    <div class="jc-trees-content">
+      <role-list class="jc-main-container-warp"></role-list>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-  name: 'SystemRoleIndex'
+  name: 'SystemRoleIndex',
+  components: {
+    OrgTrees: () => import('@/bundles/systemBundle/organization/modules/orgTrees'),
+    RoleList: () => import('./modules/roles')
+  },
+  methods: {
+    nodeChange(data) {
+      console.log('角色管理，树节点change：', data)
+    }
+  }
 }
 </script>
+<style lang="scss" scoped>
+.jc-trees-warp {
+  position: absolute;
+  top: $jc-default-dis;
+  left: $jc-default-dis;
+  bottom: $jc-default-dis;
+  width: $jc-trees-width;
+  background-color: $jc-color-white;
+  border-radius: $jc-border-radius-base;
+  z-index: 1;
+}
+.jc-trees-content {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  padding-left: $jc-trees-width + $jc-default-dis;
+  overflow: auto;
+  .jc-main-container-warp {
+    min-width: $jc-min-width - $jc-trees-width;
+  }
+}
+</style>
---
+++ src/bundles/systemBundle/role/modules/tabFilter/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-card class="jc-tabfilter-card">
+    <div slot="header" class="jc-card-title">条件搜索</div>
+    <el-form :inline="true" :model="form" class="jc-tabfilter-form" size="small">
+      <el-form-item label="审批人">
+        <el-input v-model="form.user" placeholder="审批人"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-select v-model="form.region" placeholder="活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" @click="onSubmit">查询</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+<script>
+export default {
+  name: 'SystemRoleFilter',
+  data() {
+    return {
+      form: {
+
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit('filter', this.form)
+    }
+  }
+}
+</script>
