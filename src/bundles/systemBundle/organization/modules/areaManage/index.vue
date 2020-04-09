6869c67012b73c5f7beb2f2e7b925929cb0fdf6b
yangchao <395709515@qq.com>
2020-04-09 17:05:16 +0800

    修改信息

--- src/bundles/systemBundle/organization/modules/areaManage/index.vue
+++ src/bundles/systemBundle/organization/modules/areaManage/index.vue
@@ -1,10 +1,39 @@
 <template>
-  <div class="jc-card">
-    <div></div>
+  <div class="jc-map-warp jc-card">
+    <div class="jc-title" title="组织区域">组织区域</div>
+    <div class="jc-map-space" ref="myMap"></div>
   </div>
 </template>
 <script>
+import JcMapUtils from '@/libs/JcMapUtil'
+import { MapOptions } from '@/config/JcMapConfig'
+
 export default {
-  name: 'SystemOrganizationAreaManage'
+  name: 'SystemOrganizationAreaManage',
+  mounted() {
+    JcMapUtils.init({ ...MapOptions, source: this.$refs.myMap }, () => {
+
+    })
+  }
 }
 </script>
+<style lang="scss" scoped>
+.jc-title {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 9;
+  height: 30px;
+  line-height: 30px;
+  width: 100px;
+  text-align: center;
+}
+.jc-map-warp {
+  padding: $jc-default-dis;
+}
+.jc-map-space {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/bundles/systemBundle/organization/modules/info/index.vue
+++ src/bundles/systemBundle/organization/modules/info/index.vue
@@ -3,8 +3,9 @@
     <el-row :gutter="20">
       <el-col :span="12">组织名称： 南京市</el-col>
       <el-col :span="12">组织编码： 南京市</el-col>
-      <el-col :span="12">是否统计查看： 是</el-col>
+      <el-col :span="12">是否同级查看： 是</el-col>
       <el-col :span="12">默认接收人： --</el-col>
+      <el-col :span="12">是否设置区域： 是</el-col>
     </el-row>
   </el-card>
 </template>
