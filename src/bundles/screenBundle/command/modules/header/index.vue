89fe78aa9d9fb68b8eae0f2e436bae2a350a236f
yangchao <395709515@qq.com>
2020-05-29 14:29:28 +0800

    增加大屏更新

---
+++ src/bundles/screenBundle/command/modules/header/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="jc-screen-header">
+    <div class=""></div>
+    <div class="jc-time"></div>
+    <div class="jc-screen-title">南京市常态管控指挥大屏</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ScreenCommandHeader',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
