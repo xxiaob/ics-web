fa505e983690fd578cf76284e67e9b0258209778
yangchao <395709515@qq.com>
2020-06-04 20:47:55 +0800

    更新指挥大屏用户操作

--- src/bundles/screenBundle/command/modules/operate/index.vue
+++ src/bundles/screenBundle/command/modules/operate/index.vue
@@ -4,33 +4,59 @@
       <i class="jc-select" title="框选"></i>
     </div>
     <div class="jc-opreate-item">
-      <i class="jc-show-area"></i>
+      <i class="jc-show-area" title="显示区域"></i>
       <div class="jc-opera-work">
         <el-checkbox-group v-model="areaType" @change="areaChange">
-          <el-checkbox label="复选框 A"></el-checkbox>
-          <el-checkbox label="复选框 B"></el-checkbox>
-          <el-checkbox label="复选框 C"></el-checkbox>
+          <el-checkbox class="jc-work-item" label="org">
+            <div class="jc-work-content">组织</div>
+          </el-checkbox>
+          <el-checkbox class="jc-work-item" v-for="item in areaTypes" :label="item.id" :key="item.id">
+            <div class="jc-work-content" :style="getIconStyle(item.icon)">{{item.name}}</div>
+          </el-checkbox>
         </el-checkbox-group>
       </div>
     </div>
     <div class="jc-opreate-item">
-      <i class="jc-show-word"></i>
+      <i class="jc-show-word" title="显示文字"></i>
+      <div class="jc-opera-work">
+        <el-checkbox-group v-model="wordType" @change="wordChange">
+          <el-checkbox class="jc-work-item" label="user">
+            <div class="jc-work-content">人员</div>
+          </el-checkbox>
+          <el-checkbox class="jc-work-item" label="org">
+            <div class="jc-work-content">组织</div>
+          </el-checkbox>
+          <el-checkbox class="jc-work-item" v-for="item in areaTypes" :label="item.id" :key="item.id">
+            <div class="jc-work-content" :style="getIconStyle(item.icon)">{{item.name}}</div>
+          </el-checkbox>
+        </el-checkbox-group>
+      </div>
     </div>
     <div class="jc-opreate-item">
-      <i class="jc-together"></i>
+      <i class="jc-together" title="自动聚合"></i>
+      <div class="jc-opera-work">
+        <el-checkbox-group v-model="togetherType" @change="togetherChange">
+          <el-checkbox class="jc-work-item" label="user">
+            <div class="jc-work-content">人员</div>
+          </el-checkbox>
+          <el-checkbox class="jc-work-item" v-for="item in areaTypes" :label="item.id" :key="item.id">
+            <div class="jc-work-content" :style="getIconStyle(item.icon)">{{item.name}}</div>
+          </el-checkbox>
+        </el-checkbox-group>
+      </div>
     </div>
     <div class="jc-opreate-item">
-      <i class="jc-temporary-tasks"></i>
+      <i class="jc-temporary-tasks" title="任务下发"></i>
     </div>
     <div class="jc-opreate-item">
-      <i class="jc-map-switch"></i>
+      <i class="jc-map-switch" title="地图切换"></i>
     </div>
     <div class="jc-opreate-item">
-      <i class="jc-message" @click="messageChange('CommandMessage')"></i>
+      <i class="jc-message" @click="messageChange('CommandMessage')" title="任务&问题&事件"></i>
       <span class="jc-num-tip" v-if="messageVal > 0" v-text="messageVal < 100 ? messageVal : '99'"></span>
     </div>
     <div class="jc-opreate-item">
-      <i class="jc-talk" @click="messageChange('ImTalk')"></i>
+      <i class="jc-talk" @click="messageChange('ImTalk')" title="聊天消息"></i>
       <span class="jc-num-tip" v-if="talkVal > 0" v-text="talkVal < 100 ? talkVal : '99'"></span>
     </div>
   </div>
@@ -46,8 +72,9 @@ export default {
       messageVal: 99,
       talkVal: 99,
       areaTypes: [], //存储所有 网格类型数组
-      areaType: [], //选择显示区域范围的类型
-      wordType: [] //存储选择显示文字的网格类型
+      areaType: ['org'], //选择显示区域范围的类型
+      wordType: [], //存储选择显示文字的网格类型
+      togetherType: [] //存储自动聚合的类型
     }
   },
   created() {
@@ -56,16 +83,20 @@ export default {
   methods: {
     async initData() {
       try {
-        let areaResults = await areaTypeList({})
+        const areaResults = await areaTypeList({})
 
         let areaTypes = []
+
+        let allCheckIds = ['user']
 
         if (areaResults && areaResults.length) {
           areaResults.forEach(item => {
             areaTypes.push({ id: item.areaTypeId, name: item.areaTypeName, icon: item.icon })
+            allCheckIds.push(item.areaTypeId)
           })
         }
         this.areaTypes = areaTypes
+        this.togetherType = allCheckIds
       } catch (error) {
         console.log(error)
       }
@@ -83,6 +114,9 @@ export default {
     },
     wordChange() {
       //文字显示切换
+    },
+    togetherChange() {
+      //聚合显示切换
     }
   }
 }
