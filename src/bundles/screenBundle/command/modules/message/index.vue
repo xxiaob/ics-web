bdf19d615c17cfdd32d008fd5b528d1e3d41ad73
yangchao <395709515@qq.com>
2020-06-08 08:46:33 +0800

    提交大屏任务更新

--- src/bundles/screenBundle/command/modules/message/index.vue
+++ src/bundles/screenBundle/command/modules/message/index.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="jc-screen-message">wewww</div>
+  <div class="jc-screen-message">
+    <transition-group name="jc-list" tag="div" class="jc-message-content">
+      <div class="jc-message-item" v-for="(item,index) in list" :key="item.id" @click="manage(index,item)">{{'yangchao' + item.id}}</div>
+    </transition-group>
+  </div>
 </template>
 <script>
+import { getRandomNum } from '@/libs/util'
 export default {
   name: 'ScreenCommandMessage',
   props: ['options'],
   data() {
     return {
-      visible: true
+      visible: true,
+      index: 3,
+      list: [{ id: 1, type: '1' }, { id: 2, type: '2' }]
     }
   },
   mounted() {
@@ -16,6 +23,18 @@ export default {
   methods: {
     initData() {
 
+    },
+    manage(index, item) {
+      if (index % 2) {
+        this.list.splice(index, 1)
+      } else {
+        let addList = []
+
+        for (let i = 1; i < getRandomNum(2, 6); i++) {
+          addList.splice(0, 0, { id: this.index++, type: i % 3 })
+        }
+        this.list = [...addList, ...this.list]
+      }
     }
   },
   activated() {
