bd12fb564a8e8d0dfe4682fe742bc6a2562574f6
yangchao <395709515@qq.com>
2020-06-22 19:20:43 +0800

    增加接入

---
+++ src/bundles/projectBundle/muckSpecial/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="jc-main-full-container">
+    <iframe src="http://36.153.138.122:10002/eUrbanMIS/construction.htm#MontiorMgr" class="jc-muck-warp"></iframe>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProjectMuckSpecial'
+}
+</script>
+<style lang="scss" scoped>
+.jc-muck-warp {
+  display: block;
+  border: none;
+  width: 100%;
+  height: 100%;
+}
+</style>
