7474ae33dfed23f4e7fdd85bf27a09d54b811405
yangchao <395709515@qq.com>
2020-06-23 16:39:42 +0800

    提交更新

--- src/bundles/screenBundle/command/modules/gridDetail/index.vue
+++ src/bundles/screenBundle/command/modules/gridDetail/index.vue
@@ -1,5 +1,6 @@
 <template>
   <view-warp :title="options.areaName">
+    <view-tabs v-model="tabActive" :options="tabs"></view-tabs>
     <div class="jc-view-content"></div>
   </view-warp>
 </template>
@@ -8,7 +9,14 @@ export default {
   name: 'ScreenCommandGridDetail',
   props: ['options', 'project'],
   components: {
-    ViewWarp: () => import('../common/viewWarp')
+    ViewWarp: () => import('../common/viewWarp'),
+    ViewTabs: () => import('../common/viewTabs')
+  },
+  data() {
+    return {
+      tabActive: '',
+      tabs: [{ label: '基础信息', value: '1' }, { label: '任务', value: '2' }, { label: '考勤', value: '3' }]
+    }
   }
 }
 </script>
