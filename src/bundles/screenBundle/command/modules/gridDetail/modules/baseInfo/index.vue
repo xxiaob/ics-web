1548412bce821c7d845c6927c1d5446620a24840
yangchao <395709515@qq.com>
2020-06-24 17:14:21 +0800

    更新网格详情

--- src/bundles/screenBundle/command/modules/gridDetail/modules/baseInfo/index.vue
+++ src/bundles/screenBundle/command/modules/gridDetail/modules/baseInfo/index.vue
@@ -1,8 +1,30 @@
 <template>
-  <div class="jc-view-content"></div>
+  <div class="jc-view-content" v-loading="loading" element-loading-background="rgba(0, 0, 0, 0)">
+    <div class="jc-detail-warp">
+      <div class="jc-detail-label">所属组织</div>
+      <div class="jc-detail-content">南京市</div>
+    </div>
+    <div class="jc-detail-warp">
+      <div class="jc-detail-label">网格名称</div>
+      <div class="jc-detail-content">南京市</div>
+    </div>
+    <div class="jc-detail-warp">
+      <div class="jc-detail-label">网格类型</div>
+      <div class="jc-detail-content">南京市</div>
+    </div>
+    <div class="jc-detail-warp">
+      <div class="jc-detail-label">网格类型</div>
+      <div class="jc-detail-content">描述</div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-  name: 'ScreenCommandGridDetailBaseInfo'
+  name: 'ScreenCommandGridDetailBaseInfo',
+  data() {
+    return {
+      loading: false
+    }
+  }
 }
 </script>
