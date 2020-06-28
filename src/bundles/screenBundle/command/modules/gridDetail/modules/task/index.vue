90613405687c8245354a6138176b420aec131f04
yangchao <395709515@qq.com>
2020-06-28 17:31:03 +0800

    增加岗点详情和岗点任务

--- src/bundles/screenBundle/command/modules/gridDetail/modules/task/index.vue
+++ src/bundles/screenBundle/command/modules/gridDetail/modules/task/index.vue
@@ -3,6 +3,31 @@
 </template>
 <script>
 export default {
-  name: 'ScreenCommandGridDetailTask'
+  name: 'ScreenCommandGridDetailTask',
+  props: ['options', 'project'],
+  data() {
+    return {
+      loading: false
+    }
+  },
+  watch: {
+    options() {
+      this.initData()
+    }
+  },
+  created() {
+    this.initData()
+  },
+  methods: {
+    async initData() {
+      this.loading = true
+      try {
+        //
+      } catch (error) {
+        console.log(error)
+      }
+      this.loading = false
+    }
+  }
 }
 </script>
