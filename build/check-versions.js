73308846cd4558235e7167a5248cc7f5ad07e8dd
yangchao <395709515@qq.com>
2020-07-13 17:28:13 +0800

    更新配置

--- build/check-versions.js
+++ build/check-versions.js
@@ -1,10 +1,10 @@
-'use strict'
+
 const chalk = require('chalk')
 const semver = require('semver')
 const packageConfig = require('../package.json')
 const shell = require('shelljs')
 
-function exec (cmd) {
+function exec(cmd) {
   return require('child_process').execSync(cmd).toString().trim()
 }
 
@@ -45,6 +45,7 @@ module.exports = function () {
 
     for (let i = 0; i < warnings.length; i++) {
       const warning = warnings[i]
+
       console.log('  ' + warning)
     }
 
