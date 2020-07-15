01ffd648ef342fe3948199a43487e272508454dc
yangchao <395709515@qq.com>
2020-07-15 11:42:15 +0800

    更新图标，增加吸附显示

--- src/bundles/systemBundle/organization/modules/areaManage/modules/mixins/customAreaMixins.js
+++ src/bundles/systemBundle/organization/modules/areaManage/modules/mixins/customAreaMixins.js
@@ -1,9 +1,14 @@
 /**自动设置区域 */
-import { JcMapEditor } from '@/map'
-import { signEditDataFormat } from '@/libs/apiFormat'
+import { JcMapEditor, JcMapSign } from '@/map'
+import { apiBoundariesFormat, signEditDataFormat } from '@/libs/apiFormat'
+import { areaList } from '@/api/area'
+import { AREAS_TYPE, AREAS_SEARCH_TYPE } from '@/constant/CONST'
+
 let myJcMap //承载JcMap对象
 
 let myJcMapEditor //承载编辑体
+
+let parentSign //记录父级标记
 
 export default {
   methods: {
@@ -40,6 +45,46 @@ export default {
       if (myJcMapEditor) {
         myJcMapEditor.destroy()
         myJcMapEditor = null
+        parentSign = null
+      }
+    },
+    async showParentView() {
+      //显示或隐藏父级区域，如果父级id不存在，则结束显示
+      if (!this.orgPid) {
+        return
+      }
+      if (!parentSign || parentSign.pid != this.orgPid) {
+        //如果父级不存在，则进行查询
+        try {
+          let res = await areaList({ orgId: this.orgPid, orgSearchType: AREAS_TYPE.OWN, searchType: AREAS_SEARCH_TYPE.ORG })
+
+          let item = res && res.length ? res[0] : null
+
+          parentSign = {
+            pid: this.orgPid, sign: item ? new JcMapSign({
+              id: item.orgId,
+              map: myJcMap,
+              name: item.areaName,
+              tipVisible: false,
+              center: item.center.split(','),
+              boundaries: apiBoundariesFormat(item)
+            }) : null, show: false
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      //控制显示隐藏
+      parentSign.show = !parentSign.show
+      if (!parentSign.sign) {
+        return
+      }
+      if (parentSign.show) {
+        parentSign.sign.show()
+        myJcMapEditor.addAdsorbPolygons(parentSign.sign)
+      } else {
+        myJcMapEditor.removeAdsorbPolygons(parentSign.sign)
+        parentSign.sign.hide()
       }
     },
     customAreaReset() {
