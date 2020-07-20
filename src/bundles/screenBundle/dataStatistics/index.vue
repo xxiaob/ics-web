4e3a99c55e6fa95bfb5f01e11bc49844adf5ae3f
yangchao <395709515@qq.com>
2020-07-20 14:09:20 +0800

    提交内容修改

--- src/bundles/screenBundle/dataStatistics/index.vue
+++ src/bundles/screenBundle/dataStatistics/index.vue
@@ -7,11 +7,30 @@
   </section>
 </template>
 <script>
+import { projectGet } from '@/api/projects'
 import ScreenHeader from './modules/header' //顶部header
 
 export default {
   name: 'ScreenDataStatistics',
-  components: { ScreenHeader }
+  components: { ScreenHeader },
+  data() {
+    return {
+      project: { projectId: '', projectName: '', orgId: '', projectType: '' }
+    }
+  },
+  mounted() {
+    this.initData() //初始化基本内容
+  },
+  methods: {
+    async initData() {
+      if (this.$route.params.projectId) {
+        //处理项目，如果项目id存在则获取项目详情
+        let { projectId, projectName, orgId, projectType } = await projectGet(this.$route.params.projectId)
+
+        this.project = { projectId, projectName, orgId, projectType }
+      }
+    }
+  }
 }
 </script>
 <style lang="scss" scoped src="./css/datastatistics.scss"></style>
