4556798f7ac4abca18c38cfab11c605b2b4808b4
lixiangyu <654762937@qq.com>
2020-07-22 17:27:31 +0800

    投屏 音视频布局

--- src/bundles/screenBundle/dataStatistics/modules/centerContent/modules/screenProjection/index.vue
+++ src/bundles/screenBundle/dataStatistics/modules/centerContent/modules/screenProjection/index.vue
@@ -3,6 +3,8 @@
     <div class="jc-screen-space" v-show="show">
       <jc-live v-show="showModule===1"></jc-live>
       <jc-event v-show="showModule===2"></jc-event>
+      <jc-question v-show="showModule===3"></jc-question>
+      <jc-task v-show="showModule===4"></jc-task>
     </div>
   </transition>
 
@@ -11,9 +13,11 @@
 <script>
 import JcLive from './modules/live'
 import JcEvent from './modules/event'
+import JcQuestion from './modules/question'
+import JcTask from './modules/task'
 export default {
   name: 'ScreenDataCenterContentScreenProjection',
-  components: { JcLive, JcEvent },
+  components: { JcLive, JcEvent, JcQuestion, JcTask },
   data() {
     return {
       show: false,
@@ -21,9 +25,9 @@ export default {
     }
   },
   created() {
-    // setTimeout(()=>{
-    //   this.show = true
-    // }, 2000)
+    setTimeout(()=>{
+      this.show = true
+    }, 2000)
     // setTimeout(()=>{
     //   this.show = false
     // }, 4000)
--- src/bundles/screenBundle/dataStatistics/modules/centerContent/modules/screenProjection/modules/live/index.vue
+++ src/bundles/screenBundle/dataStatistics/modules/centerContent/modules/screenProjection/modules/live/index.vue
@@ -5,6 +5,10 @@
       <div class="live" @click="checkBigLive(user.userId)" :class="{audio:inviteType==='0'||inviteType==='4','big-live':bigLiveId===user.userId}" v-for="user in users" :key="user.userId" :id="user.userId">
         <div class="userName">{{user.userName}}</div>
       </div>
+      <div class="live">1</div>
+      <div class="live">2</div>
+      <div class="live">3</div>
+      <div class="live">4</div>
     </div>
   </div>
 </template>
@@ -19,6 +23,11 @@ export default {
   computed: {
     ...mapState(['user'])
   },
+  data() {
+    return {
+      users: []
+    }
+  },
   mounted() {
     if (this.live) {
       console.log('直播客户端已经初始化')
@@ -27,6 +36,10 @@ export default {
     }
   },
   methods: {
+    //切换小视频为大视频
+    checkBigLive(val) {
+      this.bigLiveId = val
+    },
     //加入频道
     join(channelId) {
       this.live.joinChannel(channelId, 'audience', false, false)
@@ -44,5 +57,55 @@ export default {
 <style lang="scss" scoped>
 .jc-live {
   height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  box-sizing: border-box;
+  position: relative;
+  .big-box {
+    width: 68%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+
+  .live-in {
+    width: 30%;
+    white-space: normal;
+    box-sizing: border-box;
+    float: right;
+    height: 100%;
+    .live {
+      display: block;
+      margin-bottom: 5px;
+      cursor: pointer;
+      height: 30%;
+    }
+
+    .big-live {
+      animation: big-live-to-full 0.3s linear;
+
+      width: 600px;
+      height: 440px;
+      position: absolute;
+      left: 10px;
+      top: 44px;
+      z-index: 999;
+    }
+  }
+}
+
+@keyframes big-live-to-full {
+  0% {
+    height: 150px;
+    width: 200px;
+    left: 600px;
+  }
+
+  100% {
+    width: 600px;
+    height: 440px;
+    left: 5px;
+  }
 }
 </style>
