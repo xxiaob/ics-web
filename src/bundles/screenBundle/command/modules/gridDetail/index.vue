c52074f457e443fdfdcea86e956c0d293a2b9643
yangchao <395709515@qq.com>
2020-07-27 10:40:33 +0800

    联调岗点一键采集

--- src/bundles/screenBundle/command/modules/gridDetail/index.vue
+++ src/bundles/screenBundle/command/modules/gridDetail/index.vue
@@ -15,12 +15,13 @@ export default {
     ViewTabs: () => import('../common/viewTabs'),
     BaseInfo: () => import('./modules/baseInfo'), //基础信息
     GridTask: () => import('./modules/task'), //任务 问题 事件
-    WorkCheck: () =>import('./modules/workCheck') //考勤
+    WorkCheck: () =>import('./modules/workCheck'), //考勤
+    GridResources: ()=> import('./modules/resources') //岗点资源，目前显示一键采集的内容
   },
   data() {
     return {
       tabComponent: 'BaseInfo',
-      tabs: [{ label: '基础信息', value: 'BaseInfo' }, { label: '综合', value: 'GridTask' }, { label: '考勤', value: 'WorkCheck' }]
+      tabs: [{ label: '基础信息', value: 'BaseInfo' }, { label: '综合', value: 'GridTask' }, { label: '考勤', value: 'WorkCheck' }, { label: '一键采集', value: 'GridResources' }]
     }
   }
 }
