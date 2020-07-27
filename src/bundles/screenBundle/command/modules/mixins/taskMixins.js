27ca67783d8793c7797d33cf28b414428f7edb1c
yangchao <395709515@qq.com>
2020-07-27 14:27:46 +0800

    增加临时任务显示和报警

--- src/bundles/screenBundle/command/modules/mixins/taskMixins.js
+++ src/bundles/screenBundle/command/modules/mixins/taskMixins.js
@@ -24,40 +24,65 @@ export default {
     async initMapTask(org) {
       //初始化
       try {
-        let res = await getScreenTask({ orgId: org.orgId, projectId: this.project.projectId })
+        let tasks = await getScreenTask({ orgId: org.orgId, projectId: this.project.projectId })
+
+        if (tasks && tasks.length) {
+          tasks.forEach(item => {
+            this.drawTask({ taskId: item.businessKey, taskName: item.taskName, center: item.position.split(',') })
+          })
+        }
       } catch (error) {
         console.log(error)
       }
       this.$EventBus.$off('org-change', this.initMapTask) //获取完成
     },
-    mapTaskChange(data) {
+    drawTask(task) {
       let myJcMap = this.getMyJcMap() //获取地图对象
 
+      let marker = new JcMapMarker({ map: myJcMap, name: task.taskName, icon: '/static/mapIcons/temporarytasks.gif', position: task.center, mapStyle: markerStyle.CENTER_MARKER })
+
+      //添加监听
+      marker.on(MAP_EVENT.CLICK, () => {
+        this.$EventBus.$emit('view-component-change', { component: 'MessageDetail', options: { id: task.taskId, type: MESSAGE_TYPE.TEMPORARY } }) //通知窗口改变
+      })
+
+      userTasks[task.taskId] = { taskId: task.taskId, center: task.center, marker }
+    },
+    mapTaskChange(data) {
       if (data.type == 1) {
         //临时任务考勤状态更新
         if (data.tasks && data.tasks.length) {
           data.tasks.forEach(item => {
-            /**
-             * 如果地图有该任务报警，但是状态恢复正常，则取消显示
-             * 如果地图该任务报警不存在，且任务需要报警，则进行显示
-             */
-            if (userTasks[item.taskId]) {
+            let index = this.abnormalTaskIds.indexOf(item.taskId)
+
+            //如果任务正常，从异常列表移除，恢复任务显示，如果不正常，则去显示异常任务标识
+            if (index > -1) {
               if (item.status == 0) {
-                userTasks[item.taskId].marker.hide()
-                delete userTasks[item.taskId]
+                this.abnormalTaskIds.splice(index, 1)
               }
             } else if (item.status == 1) {
-              let center = item.position.split(',')
+              this.abnormalTaskIds.push(item.taskId)
+              this.$EventBus.$emit('map-voice-alert', { type: VOICE_TYPE.TEMPORARY_ABNORMAL }) //通知播放提示音
+            }
+            //如果该地图对象存在，则去设置显示
+            if (userTasks[item.taskId]) {
+              let marker = userTasks[item.taskId].marker
 
-              let marker = new JcMapMarker({ map: myJcMap, name: item.taskName, icon: '/static/mapIcons/temporarytaskswarn.gif', position: center, mapStyle: markerStyle.CENTER_MARKER })
-
-              //添加监听
-              marker.on(MAP_EVENT.CLICK, () => {
-                this.$EventBus.$emit('view-component-change', { component: 'MessageDetail', options: { id: item.taskId, type: MESSAGE_TYPE.TEMPORARY } }) //通知窗口改变
-              })
-              userTasks[item.taskId] = { taskId: item.taskId, center, marker }
+              marker.icon = item.status == 0 ? '/static/mapIcons/temporarytasks.gif' : '/static/mapIcons/temporarytaskswarn.gif'
+              marker.setContent()
             }
           })
+        }
+      } else if (data.type == 2) {
+        //任务结束
+        if (data.taskId && userTasks[data.taskId]) {
+          userTasks[data.taskId].marker.hide()
+          delete userTasks[data.taskId]
+        }
+      } else if (data.type == 3) {
+        //新增临时任务，如果地图该内容不存在，则去显示
+        if (!userTasks[data.businessKey]) {
+          this.drawTask({ taskId: data.businessKey, taskName: data.titleName, center: data.position.split(',') })
         }
       }
     }
