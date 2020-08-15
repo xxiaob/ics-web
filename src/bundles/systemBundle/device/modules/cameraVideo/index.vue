7c6de082c88683ab876471bc28f5cdd13fa4f1cd
yangchao <395709515@qq.com>
2020-08-15 15:36:03 +0800

    增加videojs数据

--- src/bundles/systemBundle/device/modules/cameraVideo/index.vue
+++ src/bundles/systemBundle/device/modules/cameraVideo/index.vue
@@ -12,8 +12,6 @@
 </template>
 <script>
 import { getRelay } from '@/api/device'
-import videojs from 'video.js'
-import 'video.js/dist/video-js.css'
 
 export default {
   name: 'SystemDeviceCameraVideo',
