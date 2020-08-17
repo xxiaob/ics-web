66dc80853d3ebac0a771191ca7615015624429d5
fujie <3242451699@qq.com>
2020-08-17 15:52:27 +0800

    指挥大屏问题优化

--- src/bundles/screenBundle/command/modules/deviceDetail/modules/workingInfo/index.vue
+++ src/bundles/screenBundle/command/modules/deviceDetail/modules/workingInfo/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="jc-view-content" v-loading="loading" element-loading-background="rgba(0, 0, 0, 0)">
+  <div class="jc-view-content" element-loading-background="rgba(0, 0, 0, 0)">
     <div class="jc-detail-warp">
       <div class="jc-detail-label">在线状态：</div>
       <div class="jc-detail-content" v-text="deviceDetaillData && deviceDetaillData.online ? '在线':'离线'"></div>
@@ -11,7 +11,7 @@
     <div class="jc-detail-warp" v-if="deviceDetaillData && deviceDetaillData.online">
       <div class="jc-detail-label">视频信息：</div>
       <div class="jc-detail-content">
-        <el-button @click="videoplay(deviceDetaillData && deviceDetaillData)">点击</el-button>
+        <i class="iconfont iconshipin" @click="videoplay"></i>
       </div>
 
     </div>
