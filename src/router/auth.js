ce91e46134f1bc0f02e9f34d64d436e5b6ca7ef1
yangchao <395709515@qq.com>
2020-09-10 10:36:14 +0800

    增加快捷登录

--- src/router/auth.js
+++ src/router/auth.js
@@ -7,4 +7,12 @@ export default [{
     title: '登录',
     ignore: true
   }
+}, {
+  path: 'fastlogin',
+  name: 'fastLogin',
+  component: () => import('@/bundles/authBundle/fastAuth'),
+  meta: {
+    title: '快速登录',
+    ignore: true
+  }
 }]
